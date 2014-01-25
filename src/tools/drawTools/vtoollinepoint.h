757a4dc8fe56cc2666ffdb0f74546ed957737b5e
dismine <dismine@gmail.com>
2014-01-25 16:01:57 +0200

    Documentation for VToolLinePoint class.
    --HG--
    branch : develop

--- src/tools/drawTools/vtoollinepoint.h
+++ src/tools/drawTools/vtoollinepoint.h
@@ -32,22 +32,22 @@
 #include "vtoolpoint.h"
 
 /**
- * @brief The VToolLinePoint class
+ * @brief The VToolLinePoint class parent for all tools what create point with line.
  */
 class VToolLinePoint : public VToolPoint
 {
     Q_OBJECT
 public:
                       /**
-                       * @brief VToolLinePoint
-                       * @param doc dom document container
-                       * @param data container with variables
-                       * @param id object id in container
+                       * @brief VToolLinePoint constructor.
+                       * @param doc dom document container.
+                       * @param data container with variables.
+                       * @param id object id in container.
                        * @param typeLine line type.
-                       * @param formula
-                       * @param basePointId
-                       * @param angle
-                       * @param parent parent object
+                       * @param formula string with length formula.
+                       * @param basePointId id base line point.
+                       * @param angle line angle.
+                       * @param parent parent object.
                        */
                       VToolLinePoint(VDomDocument *doc, VContainer *data, const qint64 &id, const QString &typeLine,
                                      const QString &formula, const qint64 &basePointId, const qreal &angle,
@@ -65,23 +65,23 @@ public slots:
     virtual void      SetFactor(qreal factor);
 protected:
     /**
-     * @brief typeLine
+     * @brief typeLine type line.
      */
     QString           typeLine;
     /**
-     * @brief formula
+     * @brief formula string with length formula.
      */
     QString           formula;
     /**
-     * @brief angle
+     * @brief angle line angle.
      */
     qreal             angle;
     /**
-     * @brief basePointId
+     * @brief basePointId id base line point.
      */
     qint64            basePointId;
     /**
-     * @brief mainLine
+     * @brief mainLine line item.
      */
     QGraphicsLineItem *mainLine;
     /**
