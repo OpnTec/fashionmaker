84fce7ec86c667334c726b4532a6a8d01c1f8bda
dismine <dismine@gmail.com>
2014-01-27 12:36:42 +0200

    Documentation for VToolTriangle class.
    --HG--
    branch : develop

--- src/tools/drawTools/vtooltriangle.h
+++ src/tools/drawTools/vtooltriangle.h
@@ -33,23 +33,23 @@
 #include "../../dialogs/dialogtriangle.h"
 
 /**
- * @brief The VToolTriangle class
+ * @brief The VToolTriangle class tool for what find point intersection two foots right triangle.
  */
 class VToolTriangle : public VToolPoint
 {
     Q_OBJECT
 public:
                    /**
-                    * @brief VToolTriangle
-                    * @param doc dom document container
-                    * @param data container with variables
-                    * @param id object id in container
-                    * @param axisP1Id
-                    * @param axisP2Id
-                    * @param firstPointId
-                    * @param secondPointId
+                    * @brief VToolTriangle constructor.
+                    * @param doc dom document container.
+                    * @param data container with variables.
+                    * @param id object id in container.
+                    * @param axisP1Id id first axis point.
+                    * @param axisP2Id id second axis point.
+                    * @param firstPointId id first triangle point, what lies on the hypotenuse.
+                    * @param secondPointId id second triangle point, what lies on the hypotenuse.
                     * @param typeCreation way we create this tool.
-                    * @param parent parent object
+                    * @param parent parent object.
                     */
                    VToolTriangle(VDomDocument *doc, VContainer *data, const qint64 &id, const qint64 &axisP1Id,
                                  const qint64 &axisP2Id, const qint64 &firstPointId, const qint64 &secondPointId,
@@ -59,27 +59,27 @@ public:
      */
     virtual void   setDialog();
     /**
-     * @brief Create help create tool
-     * @param dialog
+     * @brief Create help create tool from GUI.
+     * @param dialog dialog.
      * @param scene pointer to scene.
-     * @param doc dom document container
-     * @param data container with variables
+     * @param doc dom document container.
+     * @param data container with variables.
      */
     static void    Create(QSharedPointer<DialogTriangle> &dialog, VMainGraphicsScene  *scene, VDomDocument *doc,
                           VContainer *data);
     /**
-     * @brief Create help create tool
+     * @brief Create help create tool.
      * @param _id tool id, 0 if tool doesn't exist yet.
      * @param pointName point name.
-     * @param axisP1Id
-     * @param axisP2Id
-     * @param firstPointId
-     * @param secondPointId
+     * @param axisP1Id id first axis point.
+     * @param axisP2Id id second axis point.
+     * @param firstPointId id first triangle point, what lies on the hypotenuse.
+     * @param secondPointId id second triangle point, what lies on the hypotenuse.
      * @param mx label bias x axis.
      * @param my label bias y axis.
      * @param scene pointer to scene.
-     * @param doc dom document container
-     * @param data container with variables
+     * @param doc dom document container.
+     * @param data container with variables.
      * @param parse parser file mode.
      * @param typeCreation way we create this tool.
      */
@@ -88,18 +88,15 @@ public:
                           VMainGraphicsScene *scene, VDomDocument *doc, VContainer *data,
                           const Document::Documents &parse, const Tool::Sources &typeCreation);
     /**
-     * @brief FindPoint
-     * @param axisP1
-     * @param axisP2
-     * @param firstPoint
-     * @param secondPoint
-     * @return
+     * @brief FindPoint find point intersection two foots right triangle.
+     * @param axisP1 first axis point.
+     * @param axisP2 second axis point.
+     * @param firstPoint first triangle point, what lies on the hypotenuse.
+     * @param secondPoint second triangle point, what lies on the hypotenuse.
+     * @return point intersection two foots right triangle.
      */
     static QPointF FindPoint(const QPointF &axisP1, const QPointF &axisP2, const QPointF &firstPoint,
                              const QPointF &secondPoint);
-    /**
-     * @brief ToolType
-     */
     static const QString ToolType;
 public slots:
     /**
@@ -107,10 +104,14 @@ public slots:
      */
     virtual void   FullUpdateFromFile();
     /**
-     * @brief FullUpdateFromGui  refresh tool data from change options.
-     * @param result
+     * @brief FullUpdateFromGui refresh tool data from change options.
+     * @param result result working dialog.
      */
     virtual void   FullUpdateFromGui(int result);
+    /**
+     * @brief ShowContextMenu show context menu.
+     * @param event context menu event.
+     */
     virtual void   ShowContextMenu(QGraphicsSceneContextMenuEvent *event);
 protected:
     /**
@@ -119,7 +120,7 @@ protected:
     virtual void   RemoveReferens();
     /**
      * @brief contextMenuEvent handle context menu events.
-     * @param event
+     * @param event context menu event.
      */
     virtual void   contextMenuEvent ( QGraphicsSceneContextMenuEvent * event );
     /**
@@ -133,23 +134,23 @@ protected:
 private:
     Q_DISABLE_COPY(VToolTriangle)
     /**
-     * @brief axisP1Id
+     * @brief axisP1Id id first axis point.
      */
     qint64         axisP1Id;
     /**
-     * @brief axisP2Id
+     * @brief axisP2Id id second axis point.
      */
     qint64         axisP2Id;
     /**
-     * @brief firstPointId
+     * @brief firstPointId id first triangle point, what lies on the hypotenuse.
      */
     qint64         firstPointId;
     /**
-     * @brief secondPointId
+     * @brief secondPointId id second triangle point, what lies on the hypotenuse.
      */
     qint64         secondPointId;
     /**
-     * @brief dialogTriangle
+     * @brief dialogTriangle dialog.
      */
     QSharedPointer<DialogTriangle> dialogTriangle;
 };
