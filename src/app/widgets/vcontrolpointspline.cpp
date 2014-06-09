970d4d1f00874e430cf8901590e97bd8e2512e91
dismine <dismine@gmail.com>
2014-06-09 21:27:18 +0300

    Fix radius spline control point.
    --HG--
    branch : feature

--- src/app/widgets/vcontrolpointspline.cpp
+++ src/app/widgets/vcontrolpointspline.cpp
@@ -36,10 +36,11 @@
 VControlPointSpline::VControlPointSpline(const qint32 &indexSpline, SplinePoint::Position position,
                                          const QPointF &controlPoint, const QPointF &splinePoint,
                                          QGraphicsItem *parent)
-    :QGraphicsEllipseItem(parent), radius(qApp->toPixel(1.5)), controlLine(nullptr), indexSpline(indexSpline),
+    :QGraphicsEllipseItem(parent), radius(0), controlLine(nullptr), indexSpline(indexSpline),
       position(position)
 {
     //create circle
+    radius = (1.5/*mm*/ / 25.4) * VApplication::PrintDPI;
     QRectF rec = QRectF(0, 0, radius*2, radius*2);
     rec.translate(-rec.center().x(), -rec.center().y());
     this->setRect(rec);
