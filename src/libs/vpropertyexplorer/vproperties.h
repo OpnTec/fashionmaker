bfd2faae1fa2131d28f765881c23bb16740cbb2e
dismine <dismine@gmail.com>
2014-09-10 18:09:59 +0300

    License for library VPropertyExplorer.
    --HG--
    branch : develop

--- src/libs/vpropertyexplorer/vproperties.h
+++ src/libs/vpropertyexplorer/vproperties.h
@@ -1,28 +1,20 @@
 /************************************************************************
  **
  **  @file   vproperties.h
- **  @author Roman Telezhynskyi <dismine(at)gmail.com>
- **  @date   28 8, 2014
+ **  @author hedgeware <internal(at)hedgeware.net>
+ **  @date
  **
  **  @brief
  **  @copyright
- **  This source code is part of the Valentine project, a pattern making
- **  program, whose allow create and modeling patterns of clothing.
- **  Copyright (C) 2014 Valentina project
- **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **  All rights reserved. This program and the accompanying materials
+ **  are made available under the terms of the GNU Lesser General Public License
+ **  (LGPL) version 2.1 which accompanies this distribution, and is available at
+ **  http://www.gnu.org/licenses/lgpl-2.1.html
  **
- **  Valentina is free software: you can redistribute it and/or modify
- **  it under the terms of the GNU General Public License as published by
- **  the Free Software Foundation, either version 3 of the License, or
- **  (at your option) any later version.
- **
- **  Valentina is distributed in the hope that it will be useful,
+ **  This library is distributed in the hope that it will be useful,
  **  but WITHOUT ANY WARRANTY; without even the implied warranty of
- **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- **  GNU General Public License for more details.
- **
- **  You should have received a copy of the GNU General Public License
- **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ **  Lesser General Public License for more details.
  **
  *************************************************************************/
 
