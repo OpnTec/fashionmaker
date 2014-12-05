1cf19a8e3bd2f7c70b5f4e68265039c2a1bb6d2c
dismine <dismine@gmail.com>
2014-12-05 15:32:04 +0200

    Don't compress qm files.
    --HG--
    branch : develop

--- scripts/lrelease.sh
+++ scripts/lrelease.sh
@@ -2,4 +2,4 @@
 # Run this script if you want release all translated string in program.
 # lrelease doesn't work with recursive *.pro file and without direct pointing to correct *.pro doesn't find anything.
 # Please run this script from folder <root_folder>/scripts. 
-lrelease -compress -removeidentical -nounfinished ../share/translations.pro
+lrelease -removeidentical -nounfinished ../share/translations.pro
