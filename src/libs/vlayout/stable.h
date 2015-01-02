44372c7e6b879a9d0d751e265c0ddfb7e06b295c
dismine <dismine@gmail.com>
2015-01-02 15:53:36 +0200

    Initial commit for static library VLayout.
    --HG--
    branch : feature

---
+++ src/libs/vlayout/stable.h
@@ -0,0 +1,48 @@
+/************************************************************************
+ **
+ **  @file   stable.h
+ **  @author Roman Telezhynskyi <dismine(at)gmail.com>
+ **  @date   2 1, 2015
+ **
+ **  @brief
+ **  @copyright
+ **  This source code is part of the Valentine project, a pattern making
+ **  program, whose allow create and modeling patterns of clothing.
+ **  Copyright (C) 2013 Valentina project
+ **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **
+ **  Valentina is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Valentina is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ *************************************************************************/
+
+#ifndef STABLE_H
+#define STABLE_H
+
+/* I like to include this pragma too, so the build log indicates if pre-compiled headers were in use. */
+#ifndef __clang__
+#pragma message("Compiling precompiled headers for VLayout library.\n")
+#endif
+
+/* Add C includes here */
+
+#if defined __cplusplus
+/* Add C++ includes here */
+
+#ifdef QT_CORE_LIB
+#include <QtCore>
+#endif
+
+#endif/*__cplusplus*/
+
+#endif // STABLE_H
---
+++ src/libs/vlayout/vlayout.pri
@@ -0,0 +1,10 @@
+# ADD TO EACH PATH $$PWD VARIABLE!!!!!!
+# This need for corect working file translations.pro
+
+HEADERS += \
+    $$PWD/stable.h \
+    $$PWD/vlayoutgenerator.h
+
+SOURCES += \
+    $$PWD/stable.cpp \
+    $$PWD/vlayoutgenerator.cpp
