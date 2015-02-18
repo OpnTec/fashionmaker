22aa73e308b2692c1d60203985c64c39212ae1ae
Roman Telezhynskyi <kroluku@gmail.com>
2015-02-18 15:29:44 +0200

    Web site URL in log file.
    --HG--
    branch : develop

--- src/app/core/vapplication.cpp
+++ src/app/core/vapplication.cpp
@@ -2319,6 +2319,7 @@ void VApplication::SendReport(const QString &reportName) const
     content.append(QString("Build revision:%1").arg(BUILD_REVISION)+"\r\n");
     content.append(QString("Based on Qt %2 (32 bit)").arg(QT_VERSION_STR)+"\r\n");
     content.append(QString("Built on %3 at %4").arg(__DATE__).arg(__TIME__)+"\r\n");
+    content.append(QString("Web site:http://www.valentina-project.org/ ")+"\r\n");
     content.append("\r\n");
 
     // Creating json with report
