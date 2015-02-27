bfbe2a5fc2249cdbb68bdd292864c25d71a50455
Roman Telezhynskyi <kroluku@gmail.com>
2015-02-27 12:21:09 +0200

    Fixed copyright notice for library qmuparser.
    --HG--
    branch : develop

--- src/libs/qmuparser/qmutranslation.h
+++ src/libs/qmuparser/qmutranslation.h
@@ -1,6 +1,6 @@
 /***************************************************************************************************
  **
- **  Original work Copyright (C) 2014 Roman Telezhynskyi <dismine(at)gmail.com>
+ **  Copyright (C) 2014 Roman Telezhynskyi <dismine(at)gmail.com>
  **
  **  Permission is hereby granted, free of charge, to any person obtaining a copy of this
  **  software and associated documentation files (the "Software"), to deal in the Software
