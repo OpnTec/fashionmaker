2e344b27b0fc951fcf9460a2cd3e38198921f917
Roman Telezhynskyi <kroluku@gmail.com>
2015-09-29 17:13:29 +0300

    Fix for searching the folder 'templates'.
    --HG--
    branch : develop

--- src/libs/vmisc/vtapesettings.cpp
+++ src/libs/vmisc/vtapesettings.cpp
@@ -113,7 +113,8 @@ QString VTapeSettings::StandardTablesPath() const
 //---------------------------------------------------------------------------------------------------------------------
 QString VTapeSettings::TemplatesPath() const
 {
-    const QString stPath = QStringLiteral("/tables/template");
+    const QString stPath = QStringLiteral("/tables/templates");
+    const QString unixFullPath = QStringLiteral("/usr/share/valentina/tables/templates");
 #ifdef Q_OS_WIN
     return QApplication::applicationDirPath() + stPath;
 #elif defined(Q_OS_MAC)
@@ -131,11 +132,12 @@ QString VTapeSettings::TemplatesPath() const
         }
         else
         {
-            return QStringLiteral("/usr/share/valentina/tables/template");
+            return unixFullPath;
         }
     }
 #else // Unix
     #ifdef QT_DEBUG
+        Q_UNUSED(unixFullPath);
         return QApplication::applicationDirPath() + stPath;
     #else
         QDir dir(QApplication::applicationDirPath() + stPath);
@@ -145,7 +147,7 @@ QString VTapeSettings::TemplatesPath() const
         }
         else
         {
-            return QStringLiteral("/usr/share/valentina/tables/template");
+            return unixFullPath;
         }
     #endif
 #endif
