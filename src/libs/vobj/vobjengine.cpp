c630b2e0fc1f2a056119306d4fe5bd3119c6a92e
Roman Telezhynskyi <kroluku@gmail.com>
2015-10-08 19:27:39 +0300

    Fix for build on drone.io.
    --HG--
    branch : develop

--- src/libs/vobj/vobjengine.cpp
+++ src/libs/vobj/vobjengine.cpp
@@ -31,7 +31,7 @@
 #include <QTextStream>
 #include <QDebug>
 #if QT_VERSION < QT_VERSION_CHECK(5, 1, 0)
-#   include "../libs/vmisc/vmath.h"
+#   include "../vmisc/vmath.h"
 #else
 #   include <QtMath>
 #endif
