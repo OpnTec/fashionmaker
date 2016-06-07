fbeb56c959c897716cf63056ea68b24a4c4eec43
Roman Telezhynskyi <kroluku@gmail.com>
2016-06-07 11:49:06 +0300

    Fixed issue #501. Rename Window->Close Window to Window->Close pattern.
    --HG--
    branch : release

--- src/app/valentina/mainwindow.cpp
+++ src/app/valentina/mainwindow.cpp
@@ -2227,6 +2227,10 @@ void MainWindow::Clear()
     sceneDetails->clear();
     ArrowTool();
     comboBoxDraws->clear();
+    ui->actionDraw->setEnabled(false);
+    ui->actionDetails->setEnabled(false);
+    ui->actionLayout->setEnabled(false);
+    ui->actionNewDraw->setEnabled(false);
     ui->actionOptionDraw->setEnabled(false);
     ui->actionSave->setEnabled(false);
     ui->actionSaveAs->setEnabled(false);
@@ -3334,7 +3338,7 @@ void MainWindow::CreateActions()
     connect(ui->actionPattern_properties, &QAction::triggered, this, &MainWindow::PatternProperties);
     ui->actionPattern_properties->setEnabled(false);
     connect(ui->actionEdit_pattern_code, &QAction::triggered, this, &MainWindow::EditPatternCode);
-    connect(ui->actionCloseWindow, &QAction::triggered, this, &MainWindow::ResetWindow);
+    connect(ui->actionClosePattern, &QAction::triggered, this, &MainWindow::ResetWindow);
     connect(ui->actionShowCurveDetails, &QAction::triggered, this, &MainWindow::ActionCurveDetailsMode);
     connect(ui->actionLoadIndividual, &QAction::triggered, this, &MainWindow::LoadIndividual);
     connect(ui->actionLoadStandard, &QAction::triggered, this, &MainWindow::LoadStandard);
