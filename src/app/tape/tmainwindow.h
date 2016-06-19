ed6c72bda93ccad6ab679003e8abea71f0e1084d
Roman Telezhynskyi <kroluku@gmail.com>
2016-06-19 20:31:24 +0300

    Fixed Tape crash. (grafted from 47b3aaf84786a338bc7c31284bef0bd5017a0c67)
    --HG--
    branch : develop

--- src/app/tape/tmainwindow.h
+++ src/app/tape/tmainwindow.h
@@ -153,7 +153,7 @@ private:
     enum { MaxRecentFiles = 5 };
     QAction            *recentFileActs[MaxRecentFiles];
     QAction            *separatorAct;
-
+    QVector<QObject *> hackedWidgets;
 
     void SetupMenu();
     void InitWindow();
@@ -205,6 +205,9 @@ private:
     void CreateWindowMenu(QMenu *menu);
 
     bool IgnoreLocking(int error, const QString &path);
+
+    template <class T>
+    void HackWidget(T **widget);
 };
 
 #endif // TMAINWINDOW_H
