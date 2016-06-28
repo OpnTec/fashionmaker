412ea278fe7b38fde5aa82b88e0bc7654781ae98
Roman Telezhynskyi <kroluku@gmail.com>
2016-06-28 09:15:48 +0300

    Fixed issue #516. Valentina crashes when use "Point intersect line and axis" instrument. (grafted from e1677e1a2ae3eb9ddc41033299072c6b97fb2f56)
    --HG--
    branch : release

--- src/libs/vtools/tools/drawTools/toolpoint/toolsinglepoint/vtooltriangle.cpp
+++ src/libs/vtools/tools/drawTools/toolpoint/toolsinglepoint/vtooltriangle.cpp
@@ -186,6 +186,10 @@ QPointF VToolTriangle::FindPoint(const QPointF &axisP1, const QPointF &axisP2, c
     {
         return QPointF();
     }
+    if (axis.angle() == hypotenuse.angle() || qAbs(axis.angle() - hypotenuse.angle()) == 180)
+    {
+        return QPointF();
+    }
 
     qreal step = 1;
 
