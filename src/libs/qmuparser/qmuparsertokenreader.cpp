67614bcd0b7216dca22bd262f1ff4fd85c91926e
Roman Telezhynskyi <kroluku@gmail.com>
2016-08-08 15:07:26 +0300

    Cppcheck warnings.
    --HG--
    branch : develop

--- src/libs/qmuparser/qmuparsertokenreader.cpp
+++ src/libs/qmuparser/qmuparsertokenreader.cpp
@@ -663,7 +663,7 @@ bool QmuParserTokenReader::IsOprt ( token_type &a_Tok )
     }
 
     // Check if the operator is a built in operator, if so ignore it here
-    const QStringList pOprtDef = m_pParser->GetOprtDef();
+    const QStringList &pOprtDef = m_pParser->GetOprtDef();
     QStringList::const_iterator constIterator;
     for ( constIterator = pOprtDef.constBegin(); m_pParser->HasBuiltInOprt() && constIterator != pOprtDef.constEnd();
             ++constIterator )
