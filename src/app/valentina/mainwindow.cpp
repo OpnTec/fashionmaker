a890ec47ddf96bd7b645ec301482945c88dd6e3a
Roman Telezhynskyi <kroluku@gmail.com>
2016-08-12 17:38:01 +0300

    Fixed issue #537. Valentina crashes when use undo command. (grafted from b9726acb2bb6b7bd606d6ecc96e6498f093c1861)
    --HG--
    branch : develop

--- src/app/valentina/mainwindow.cpp
+++ src/app/valentina/mainwindow.cpp
@@ -2445,6 +2445,7 @@ void MainWindow::Clear()
     qt_ntfs_permission_lookup--; // turn it off again
 #endif /*Q_OS_WIN32*/
     qApp->getUndoStack()->clear();
+    toolOptions->ClearPropertyBrowser();
     toolOptions->itemClicked(nullptr);
 }
 
