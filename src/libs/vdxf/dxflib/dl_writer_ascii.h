049a72ff20a4798ae27170b4de885443838dc168
Roman Telezhynskyi <kroluku@gmail.com>
2016-12-20 22:55:40 +0200

    "C3861: 'snprintf': identifier not found" with VS2013 and prior.
    --HG--
    branch : develop

--- src/libs/vdxf/dxflib/dl_writer_ascii.h
+++ src/libs/vdxf/dxflib/dl_writer_ascii.h
@@ -33,6 +33,10 @@
     #if (_MSC_VER > 1000)
     #pragma once
     #endif // _MSC_VER > 1000
+
+    #if _MSC_VER < 1900
+    #define snprintf _snprintf
+    #endif
 #endif // Q_CC_MSVC
 
 #include <fstream>
