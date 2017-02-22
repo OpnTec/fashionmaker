703a26a5cf4f9a0a4d099cf0ae8dc86906ac27b3
Roman Telezhynskyi <kroluku@gmail.com>
2017-02-22 19:29:30 +0200

    Remove useless assignment to variable "date".
    --HG--
    branch : develop

--- src/libs/vlayout/vlayoutpiece.cpp
+++ src/libs/vlayout/vlayoutpiece.cpp
@@ -115,11 +115,6 @@ VLayoutPiece VLayoutPiece::Create(const VPiece &piece, const VContainer *pattern
     if (geom.IsVisible() == true)
     {
         VAbstractPattern* pDoc = qApp->getCurrentDocument();
-        QDate date;
-        if (pDoc->IsDateVisible() == true)
-        {
-            date = QDate::currentDate();
-        }
         det.SetPatternInfo(pDoc, geom, qApp->font(), pattern->size(), pattern->height());
     }
     const VGrainlineGeometry& grainlineGeom = piece.GetGrainlineGeometry();
