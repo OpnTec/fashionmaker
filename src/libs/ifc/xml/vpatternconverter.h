3442da18eeb24829cbbc10ed7e3b6b0932993faa
Roman Telezhynskyi <kroluku@gmail.com>
2017-03-10 19:14:14 +0200

    Fix pattern format version after the merge.
    --HG--
    branch : feature

--- src/libs/ifc/xml/vpatternconverter.h
+++ src/libs/ifc/xml/vpatternconverter.h
@@ -55,10 +55,10 @@ public:
 // GCC 4.6 doesn't allow constexpr and const together
 #if !defined(__INTEL_COMPILER) && !defined(__clang__) && defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) <= 406
     static Q_DECL_CONSTEXPR int PatternMinVer = CONVERTER_VERSION_CHECK(0, 1, 0);
-    static Q_DECL_CONSTEXPR int PatternMaxVer = CONVERTER_VERSION_CHECK(0, 4, 3);
+    static Q_DECL_CONSTEXPR int PatternMaxVer = CONVERTER_VERSION_CHECK(0, 4, 4);
 #else
     static Q_DECL_CONSTEXPR const int PatternMinVer = CONVERTER_VERSION_CHECK(0, 1, 0);
-    static Q_DECL_CONSTEXPR const int PatternMaxVer = CONVERTER_VERSION_CHECK(0, 4, 3);
+    static Q_DECL_CONSTEXPR const int PatternMaxVer = CONVERTER_VERSION_CHECK(0, 4, 4);
 #endif
 
 protected:
@@ -104,6 +104,7 @@ private:
     void ToV0_4_1();
     void ToV0_4_2();
     void ToV0_4_3();
+    void ToV0_4_4();
 
     void          TagUnitToV0_2_0();
     void          TagIncrementToV0_2_0();
