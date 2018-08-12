3fd4dfc17380169ec8228843af566f8052090b48
Roman Telezhynskyi <kroluku@gmail.com>
2018-08-12 10:13:59 +0300

    Port patch from FreeBSD port.
    --HG--
    branch : release

--- src/libs/ifc/xml/vabstractconverter.h
+++ src/libs/ifc/xml/vabstractconverter.h
@@ -31,7 +31,7 @@
 
 #include <qcompilerdetection.h>
 
-#if !defined(Q_OS_OSX) && !defined(Q_OS_WIN) && defined(Q_CC_GNU)
+#if !defined(Q_OS_OSX) && !defined(Q_OS_WIN) && !defined(Q_OS_FREEBSD) && defined(Q_CC_GNU)
 #include <sys/sysmacros.h>
 #endif
 
