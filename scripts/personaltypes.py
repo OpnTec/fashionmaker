70daf7c2c44a66eb70687ba25484736bb832c811
Roman Telezhynskyi <kroluku@gmail.com>
2018-10-16 14:31:29 +0300

    Improve Debug Helper scripts to support more custom types. (grafted from f7ae391d018df40da98f570920cc9d35d8aeac10)
    --HG--
    branch : develop

--- scripts/personaltypes.py
+++ scripts/personaltypes.py
@@ -11,3 +11,15 @@ def qdump__VSAPoint(d, value):
 def qdump__PieceNodeAngle(d, value):
     d.putValue(value.cast(value.type.name).displayEnum())
     d.putNumChild(0)
+
+def qdump__PassmarkAngleType(d, value):
+    d.putValue(value.cast(value.type.name).displayEnum())
+    d.putNumChild(0)
+
+def qdump__PassmarkLineType(d, value):
+    d.putValue(value.cast(value.type.name).displayEnum())
+    d.putNumChild(0)
+
+def qdump__Tool(d, value):
+    d.putValue(value.cast(value.type.name).displayEnum())
+    d.putNumChild(0)
