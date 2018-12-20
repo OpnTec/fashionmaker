e23438347aeaeb2b99d7327e813b8cfe781f028c
Roman Telezhynskyi <kroluku@gmail.com>
2018-12-20 17:50:33 +0200

    Fixed issue #912. Labels in inactive state return to initial color. (grafted from b222072d66ff164252223728d913226b500df73a)
    --HG--
    branch : develop

--- src/libs/vtools/dialogs/tools/dialogtool.cpp
+++ src/libs/vtools/dialogs/tools/dialogtool.cpp
@@ -1219,6 +1219,7 @@ void DialogTool::ChangeColor(QWidget *widget, const QColor &color)
     SCASSERT(widget != nullptr)
     QPalette palette = widget->palette();
     palette.setColor(QPalette::Active, widget->foregroundRole(), color);
+    palette.setColor(QPalette::Inactive, widget->foregroundRole(), color);
     widget->setPalette(palette);
 }
 
