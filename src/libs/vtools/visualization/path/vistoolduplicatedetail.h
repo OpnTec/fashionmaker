475bdadf9bc86d3b95ef3de32fca8ea1bb414d02
Roman Telezhynskyi <dismine@gmail.com>
2020-01-31 08:00:05 +0200

    Fix link to project repo in license header.

--- src/libs/vtools/visualization/path/vistoolduplicatedetail.h
+++ src/libs/vtools/visualization/path/vistoolduplicatedetail.h
@@ -9,7 +9,7 @@
  **  This source code is part of the Valentina project, a pattern making
  **  program, whose allow create and modeling patterns of clothing.
  **  Copyright (C) 2017 Valentina project
- **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **  <https://gitlab.com/smart-pattern/valentina> All Rights Reserved.
  **
  **  Valentina is free software: you can redistribute it and/or modify
  **  it under the terms of the GNU General Public License as published by
